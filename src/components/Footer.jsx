b6034193b8a317d795a3608d58dd545881f4bc86
kasunijayasekara <kasunikoshila@gmail.com>
2025-08-24 11:04:29 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -36,8 +36,8 @@ const Footer = () => {
           <div className='lg:col-span-4'>
             <div className='flex gap-1 items-center mb-6'>
               <div className="flex items-center gap-1 cursor-pointer">
-                <div className="w-4 h-4 bg-green-500 rounded-full opacity-75 hover:opacity-100 transition-opacity"></div>
-                <div className="w-4 h-4 bg-yellow-500 -ml-2 rounded-full opacity-100 hover:opacity-75 transition-opacity"></div>
+                <div className="w-4 h-4 bg-yellow-200 rounded-full opacity-75 hover:opacity-100 transition-opacity"></div>
+          <div className="w-4 h-4 bg-green-950 -ml-2 rounded-full opacity-100 hover:opacity-75 transition-opacity"></div>
               </div>
               <span className='text-xl font-medium ml-1'>FitLife LK</span>
             </div>
